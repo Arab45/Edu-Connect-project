70b3bda3ae076df740c590751bea76418fa80146
Arab45 <horlakunleolayiwola@gmail.com>
2024-12-17 11:42:38 +0000

    cors commit

--- src/router/profile-router.js
+++ src/router/profile-router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { createProfile, fetchAllProfile, updatedProfile, deletedProfile } = require('../controller/profile-controller');
 const { validateProfile, validation } = require('../middleware/validator');
+const { verifyLoginAdminToken } = require('../controller/admin-auth-controller');
 const router = express.Router();
 
-router.post('/create-profile',  validateProfile, validation, createProfile);
+router.post('/create-profile',  verifyLoginUserToken, validateProfile, validation, createProfile);
 router.get('/fetch-all-profile', fetchAllProfile);
 router.put('/update-profile/:id', updatedProfile);
 router.delete('/delete-profile/:id', deletedProfile);
